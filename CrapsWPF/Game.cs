03e3f77384a759f3da243b7321521b02ab1cdf9d
mizesolutions <brian@mizesolutions.com>
2017-11-04 10:30:07 -0700

    Finished Roll and Game classes

--- CrapsWPF/Game.cs
+++ CrapsWPF/Game.cs
@@ -37,9 +37,8 @@ namespace CrapsWPF
             }
             catch (ArgumentException e)
             {
-                MessageBox.Show(Application.Current.MainWindow, "Player Bank", "Player Bank can not be negative.\nStarting Player Bank at $10000.\n('cause I'm a nice guy)");
+                MessageBox.Show(Application.Current.MainWindow, "Player Bank", e + "\nStarting Player Bank at $10000.\n('cause I'm a nice guy)");
             }
-            
             house = new Player();
             roll = new Roll();
             Bet = 0;
@@ -51,7 +50,63 @@ namespace CrapsWPF
         public int DiceTotal { get => this.diceTotal; set => this.diceTotal = value; }
         public bool GameOver { get => this.gameOver; set => this.gameOver = value; }
 
+        public void RollDice()
+        {
+            dice1.RollDice();
+            dice2.RollDice();
+            DiceTotal = dice1.Value + dice2.Value;
+        }
 
+        public void CheckRoll()
+        {
+            if (roll.CheckRoll(this))
+            {
+                this.RollDice();
+                this.CheckRoll();
+            }
+            else
+                GameOver = true;
+        }
+
+        public bool CheckWin()
+        {
+            return player.Win;
+        }
+
+        public int CheckBank()
+        {
+            return player.Bank;
+        }
+
+        public void SetPoints(int type)
+        {
+            if (type == 0)
+            {
+                player.Points += 1;
+            }
+            else
+                house.Points += 1;
+        }
+
+        public void SetBank(int type, int value)
+        {
+            if(type == 0)
+            {
+                player.Bank += value;
+                house.Bank -= value;
+            }
+            else
+            {
+                house.Bank += value;
+                player.Bank -= value;
+            }
+        }
+
+        public void SetWin(bool value)
+        {
+            player.Win = value;
+            house.Win = !value;
+        }
 
     }
 }
--- CrapsWPF/Roll.cs
+++ CrapsWPF/Roll.cs
@@ -22,8 +22,59 @@ namespace CrapsWPF
 
         public bool CheckRoll(Game game)
         {
+            if(!PointActive)
+            {
+                return RegularRoll(game);
+            }
+            else
+                return PointRoll(game);
+        }
+
+        private bool RegularRoll(Game game)
+        {
+            if (game.DiceTotal != 2 || game.DiceTotal != 3 || game.DiceTotal != 7 || game.DiceTotal != 11 || game.DiceTotal != 12)
+            {
+                RollPoint = game.DiceTotal;
+                return PointActive = true;
+            }
+            else if (game.DiceTotal == 7 || game.DiceTotal == 11)
+            {
+                return PlayerWin(game);
+            }
+            else
+                return HouseWin(game);
+        }
+
+        private bool PointRoll(Game game)
+        {
+            if (game.DiceTotal != RollPoint && game.DiceTotal != 7)
+            {
+                return true;
+            }
+            else if (game.DiceTotal == RollPoint)
+            {
+                return PlayerWin(game);
+            }
+            else
+                return HouseWin(game);
+        }
+
+        private bool PlayerWin(Game game)
+        {
+            game.SetPoints(0);
+            game.SetBank(0, game.Bet);
+            game.SetWin(true);
             return false;
         }
+
+        private bool HouseWin(Game game)
+        {
+            game.SetPoints(1);
+            game.SetBank(1, game.Bet);
+            game.SetWin(false);
+            return false;
+        }
+
 
     }
 }
