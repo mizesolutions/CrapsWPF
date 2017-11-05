7a986c6bb5130b38a3cbf79efb968fc31dd8accf
mizesolutions <brian@mizesolutions.com>
2017-11-05 07:33:31 -0800

    Frontend changes

--- CrapsWPF/Properties/AssemblyInfo.cs
+++ CrapsWPF/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("31.31.1711.05")]
-[assembly: AssemblyFileVersion("31.31.1711.05")]
+[assembly: AssemblyVersion("45.45.1711.05")]
+[assembly: AssemblyFileVersion("45.45.1711.05")]
