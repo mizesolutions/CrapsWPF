c28ac01e6c5d75e0071c64fc026bc81a9e96a473
mizesolutions <brian@mizesolutions.com>
2017-11-06 06:07:27 -0800

    Building game play

--- CrapsWPF/Properties/AssemblyInfo.cs
+++ CrapsWPF/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("64.64.1711.06")]
-[assembly: AssemblyFileVersion("64.64.1711.06")]
+[assembly: AssemblyVersion("66.66.1711.06")]
+[assembly: AssemblyFileVersion("66.66.1711.06")]
